d9eddcbba131c668fba7e04f7c24f331a86676fd
hiendbui <hiendbui0@gmail.com>
2020-10-28 17:15:42 -0700

    Edit User table and create artwork table/model

--- first_routes_and_controllers/app/models/user.rb
+++ first_routes_and_controllers/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :password, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+
 end
--- first_routes_and_controllers/db/schema.rb
+++ first_routes_and_controllers/db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_221343) do
+ActiveRecord::Schema.define(version: 2020_10_28_233952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
